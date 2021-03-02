1bb678f8c6cff86951f46c4491f7b94f436f2f7d
BrianLevin <bml201095@gmail.com>
2021-03-02 10:09:49 -0500

    created input and output staement that will ask the user for the amount of change they want to enter

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ const int nickel_value = 5;
 
 int change_amount;
 
+  cout << "Enter an amount in cents : ";
+    cin >> change_amount;
+
+
 int balence, dollars, quarters, dimes, nickels, pennies;
 
 
