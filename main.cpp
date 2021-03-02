ca7cf00f36546b5ec9896e883c1df0357be85efc
BrianLevin <bml201095@gmail.com>
2021-03-02 10:01:41 -0500

    defined conversion values in cents for the factor tree

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main() {
 
+const int dollar_value = 100;
 
+const int dollar_value = 25;
+const int dollar_value = 10;
+const int dollar_value = 5;
+
+
+
+return 0;
 
 }
